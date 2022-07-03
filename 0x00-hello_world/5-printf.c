d5a48142453621d434b1662eea21a0a6094eda45
Sbusiso-tech <sibusisoshinga@gmail.com>
2022-07-03 12:41:22 -0700

    Hello 0x00

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
